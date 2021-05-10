d7f95647b7ed5f2b60a5465df11974a672d85b30
Elestupinanc <83765827+Elestupinanc@users.noreply.github.com>
2021-05-10 12:33:03 -0500

    app start

---
+++ personaADB_backend/src/main/java/co/edu/usbbog/abd/PersonaAdbBackendApplication.java
@@ -0,0 +1,13 @@
+package co.edu.usbbog.abd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PersonaAdbBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PersonaAdbBackendApplication.class, args);
+	}
+
+}
